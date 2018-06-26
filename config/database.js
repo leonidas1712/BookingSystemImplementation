8497d1ca749ca7387ba30709010c85f7dbdb5b6f
Huzaifa Raghav <windowpane1712@gmail.com>
2018-06-26 17:42:20 +0800

    changed url

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 //Created 7 Sep 2017
 
 module.exports = {
-  'url': process.env.MONGODB_URI
+  'url': 'mongodb//heroku_p0mt2gc8:vhlk2ir4l5n3a1l3tdjjjkcpis@ds119171.mlab.com:19171/heroku_p0mt2gc8'
 
 }
